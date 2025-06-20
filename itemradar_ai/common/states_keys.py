25a24b59bcc03ccaf7195fb9f4b2bea092328bfe
rubenixter <124615627+rubenixter@users.noreply.github.com>
2025-06-19 19:45:06 -0500

    Delete itemradar_ai directory

--- itemradar_ai/common/states_keys.py
+++
@@ -1,7 +0,0 @@
-class StateKeys:
-    SEARCH_PARAMS = "search_params"
-    MATCH_RESULTS = "match_results"
-    FILTERED_RESULTS = "filtered_results"
-    USER_ANSWER = "user_answer"
-    CLARIFYING_QUESTION = "clarifying_question"
-    WORKFLOW_STAGE = "workflow_stage"
